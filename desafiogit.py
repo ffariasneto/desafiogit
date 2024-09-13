a3160fd89d97c18d6a990ea071a39b2781bfd2bf
franciscofarias <francisco.fariasneto14@gmail.com>
2024-09-13 17:11:47 -0300

    Criada lista de produtos

---
+++ desafiogit.py
@@ -0,0 +1,8 @@
+print("Análise de Vendas")
+produtos = [
+    {"Nome": "Produto A", "Valor Unit": 5.50, "Quantidade": 10},
+    {"Nome": "Produto B", "Valor Unit": 6.90, "Quantidade": 32},
+    {"Nome": "Produto C", "Valor Unit": 8.69, "Quantidade": 14},
+]
+for q in produtos:
+    print(q)
